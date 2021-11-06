218233b39121d18d958addc8de9a3e0d1ce333da
unknown <albertwtyan@163.com>
2021-11-06 17:52:59 +0800

    add login response

--- main.py
+++ main.py
@@ -2,13 +2,34 @@
 
 from fastapi import FastAPI
 import uvicorn
+from pydantic import BaseModel
+from typing import Optional
+import pandas as pd
+
 
 app = FastAPI()
+
+
+class User(BaseModel):
+    userName: str
+    passward: str
 
 
 @app.get("/")
 async def main():
     return {"message": "Hello，FastAPI"}
+
+
+@app.post("/login")
+def login(user: User):
+    author = pd.read_excel("static_db/authoraztion.xlsx")
+    print(user)
+    print(author['userName'].tolist())
+    if user.userName in author['userName'].tolist():
+        if author.loc[author['userName'] == user.userName, "password"][0] == user.passward:
+            return True
+    return False
+
 
 if __name__ == '__main__':
     uvicorn.run(app, host="127.0.0.1", port=8000)
---
+++ routers/users.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter
+from pydantic import BaseModel
+from typing import Optional
+
+
+router = APIRouter()
+
+
+class User(BaseModel):
+    userName: str
+    passward: str
+
+
+@router.get("/users/", tags=["users"])
+async def read_users():
+    return [{"username": "Rick"}, {"username": "Morty"}]
+
+
+@router.get("/users/me", tags=["users"])
+async def read_user_me():
+    return {"username": "fakecurrentuser"}
+
+
+@router.get("/users/{username}", tags=["users"])
+async def read_user(username: str):
+    return {"username": username}
